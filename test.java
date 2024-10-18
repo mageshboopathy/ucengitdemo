b27eec1b8a3659c8725513d7b2a19bc2d92d5704
Magesh Boopathy <62498121+mageshboopathy@users.noreply.github.com>
2024-10-18 23:12:00 +0530

    my changes

--- test.java
+++ test.java
@@ -23,4 +23,27 @@ public class HighComplexityExample {
             }
         }
     }
+
+     public static boolean binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if the target is at mid
+            if (arr[mid] == target) {
+                return true;
+            }
+            // If target is greater, ignore the left half
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore the right half
+            else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
 }
