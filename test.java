d17abf4769993cacd337d2ab0c20ec4c52ab5a95
Magesh Boopathy <62498121+mageshboopathy@users.noreply.github.com>
2024-10-18 23:09:30 +0530

    initial push

--- test.java
+++ test.java
@@ -1,1 +1,26 @@
 
+import java.util.Arrays;
+
+public class HighComplexityExample {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 9, 1, 5, 6};
+        bubbleSort(numbers);
+
+        System.out.println("Sorted array: " + Arrays.toString(numbers));
+    }
+
+    // Bubble Sort Algorithm (O(n^2) time complexity)
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
